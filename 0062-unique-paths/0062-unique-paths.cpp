b26a7eb4b10f4cb4c8421e9ec8c97baa761e4e97
Lalit Dumka <70807825+Lalit-Dumka@users.noreply.github.com>
2023-08-19 12:38:20 +0530

    Time: 0 ms (100%), Space: 6 MB (77.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,14 @@
 class Solution {
-    int checkPath(vector<vector<int>> &dp, int m, int n, int i, int j){
-        if(i>=m || j>= n) return 0;
-        if(i==m-1 && j==n-1) return 1;
-        if(dp[i][j]) return dp[i][j];
-        return dp[i][j] = checkPath(dp, m,n,i+1,j) + checkPath(dp, m,n,i,j+1);
-        
-    }
-
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n,0));
-        return checkPath(dp,m,n,0,0);
+        int N = m+n-2;
+        int r = n-1;
+
+        long long sol = 1;
+        for(int i = 1; i<=r; i++){
+            sol= sol* (N-r+i)/i;
+        }
+
+        return sol;
     }
 };
