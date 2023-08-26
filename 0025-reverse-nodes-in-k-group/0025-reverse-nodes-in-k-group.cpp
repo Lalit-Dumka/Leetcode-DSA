5cc9c0d5ec401781ee27e7ca2a557f02996f1eaf
Lalit Dumka <70807825+Lalit-Dumka@users.noreply.github.com>
2023-08-26 11:40:37 +0530

    Time: 17 ms (36.82%), Space: 11.5 MB (48.36%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+     pair<ListNode*, ListNode*> reverseList(ListNode* head) {
+        ListNode* prev=NULL;
+        ListNode* newtail = head;
+        while(head){
+            ListNode* curr = head;
+            head= head->next;
+            curr->next = prev;
+            prev= curr;
+        }
+        return make_pair(prev, newtail );
+     }
+
+public:
+ ListNode* reverseKGroup(ListNode* head, int k) {
+   if(head==NULL || head->next==NULL) return head;
+   ListNode* newHead = NULL;
+   ListNode* curr = head;
+   ListNode* tail =head;
+   ListNode* prev = NULL;
+   int i = 1;
+   while(tail!=NULL && tail->next!=NULL && i<k){
+     tail=tail->next;
+     i++;
+
+     if(i==k){
+       ListNode* next = tail->next;
+       tail->next =NULL;
+       pair<ListNode*, ListNode*> rl =  reverseList(curr);
+       if(newHead==NULL){
+         newHead = rl.first;
+       }else{
+         prev->next = rl.first;
+       }
+      
+       prev = rl.second;
+       rl.second->next = next;
+       tail= next;
+       curr=next;
+       i=1;
+     }
+   }
+   if(newHead) return newHead;
+   return head;
+ }
+};
