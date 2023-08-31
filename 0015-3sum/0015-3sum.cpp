e1d12dcc2652dabb7118f5913b46a04839caff3b
Lalit Dumka <70807825+Lalit-Dumka@users.noreply.github.com>
2023-08-31 10:15:37 +0530

    Time: 173 ms (56.36%), Space: 25.9 MB (53.2%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,23 +4,23 @@ public:
         vector<vector<int>> sol;
         int n= nums.size();
         sort(nums.begin(), nums.end());
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; ++i){
             int left = i+1, right = n-1;
             if(i>0 && nums[i]==nums[i-1]) continue;
             while(left<right){
                 if(nums[i]+nums[left]+nums[right]==0){
                     vector<int> temp = {nums[i],nums[left],nums[right]};
                     sol.push_back(temp);
-                    left++;
-                    right--;
-                    while(left<n && nums[left]==nums[left-1]) left++;
-                    while(right>=0 && nums[right]==nums[right+1]) right--;
+                    ++left;
+                    --right;
+                    while(left<n && nums[left]==nums[left-1]) ++left;
+                    while(right>=0 && nums[right]==nums[right+1]) --right;
                 }
                 else if(nums[i]+nums[left]+nums[right]<0){
-                    left++;
+                    ++left;
                 }
                 else{
-                    right--;
+                    --right;
                 }
             }
             
