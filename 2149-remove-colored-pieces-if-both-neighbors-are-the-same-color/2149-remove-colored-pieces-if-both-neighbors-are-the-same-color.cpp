7d2a2dfc27efa142510b437f1d8b1ef4300a88cd
Lalit Dumka <70807825+Lalit-Dumka@users.noreply.github.com>
2023-10-02 08:23:47 +0530

    Time: 27 ms (93.3%), Space: 13.5 MB (33.01%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    bool winnerOfGame(string colors) {
-        map<char, int> c;
-        for (auto it = colors.begin(); it != colors.end(); ) {
-            char x = *it;
-            auto t = it;
-            while (t != colors.end() && *t == x) {
-                t++;
+    bool winnerOfGame(string color) {
+        //we first take three consecutive pointers
+        int i =0 ; 
+        int j = i+1 ;
+        int k = i + 2 ;
+        int countA =0 ;
+        int countB = 0 ;
+        while(k<color.size()){
+            if(color[i] == 'A' && color[j] == 'A' && color[k] == 'A'){
+                countA++ ; //if we a get valid move for alice
             }
-            c[x] += max(int(distance(it, t) - 2), 0);
-            it = t;
+            if(color[i] == 'B' && color[j] == 'B' && color[k] == 'B'){
+                countB++ ; //if we get a valid move for bob
+            }
+            i++ ;
+            j++ ;
+            k++ ;
         }
-
-        if (c['A'] > c['B']) {
-            return true;
+        if(countA > countB){
+            return true ;
         }
-        return false;
+        else{
+            return false ;
+        }
     }
 };
