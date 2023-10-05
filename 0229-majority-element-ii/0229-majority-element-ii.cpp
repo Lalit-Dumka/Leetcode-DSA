ab6f32caf817848249c257ad323ce468971283ac
Lalit Dumka <70807825+Lalit-Dumka@users.noreply.github.com>
2023-10-05 19:55:43 +0530

    Time: 3 ms (99.07%), Space: 16.2 MB (18.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,50 +1,28 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> sol;
-        int n = nums.size();
-        int el1 = INT_MIN, el2 = INT_MIN;
-        int c1 = 0, c2 = 0;
-
-        for(int i=0; i<n; i++){
-            if(c1 == 0 && el2 != nums[i]){
-                el1 = nums[i];
-                c1=1;
-            }
-            else if(c2 == 0 && el1 != nums[i]){
-                el2 = nums[i];
-                c2=1;
-            }
-            else if(el1 == nums[i]){
-                c1++;
-            }
-            else if(el2 == nums[i]){
-                c2++;
-            }
-            else{
-                c1--;
-                c2--;
-            }
-        }
-
-        int a=0, b=0;
-        for(int j=0; j<n; j++){
-            if(nums[j]==el1){
-                a++;
-            }
-            else if(nums[j]==el2){
-                b++;
-            }
-        }
-
-        if(a> (n/3.0)){
-            sol.push_back(el1);
-        }
-        if(b> (n/3.0)){
-            sol.push_back(el2);
-        }
-
+        // Create a frequency map to store the count of each element
+        unordered_map<int, int> elementCountMap;
         
-        return sol;
+        // Iterate through the input array to count element occurrences
+        for(int i = 0; i < nums.size(); i++) {
+            elementCountMap[nums[i]]++;
+        }
+        
+        vector<int> majorityElements;
+        int threshold = nums.size() / 3;
+        
+        // Iterate through the frequency map to identify majority elements
+        for(auto elementCountPair : elementCountMap) {
+            int element = elementCountPair.first;
+            int count = elementCountPair.second;
+            
+            // Check if the element count is greater than the threshold
+            if(count > threshold) {
+                majorityElements.push_back(element);
+            }
+        }
+        
+        return majorityElements; 
     }
 };
